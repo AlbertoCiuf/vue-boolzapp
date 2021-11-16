42df61a9729e503e415292a8ccdd4c21a61139c3
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-16 11:01:44 +0100

    aggiunta ultimo accesso nella chat aperta, data della chat nell'elenco a sinistra e preview dell'ultimo messaggio della chat nell'elenco in base alla lunghezza dello stesso

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -20,7 +20,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 16:55:08',
             text: 'Per fortuna!',
             status: 'sent'
           },
@@ -43,7 +43,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 05:55:08',
             text: 'Perfetto',
             status: 'sent'
           },
@@ -84,7 +84,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 12:34:08',
             text: 'Grande',
             status: 'sent'
           },
@@ -107,7 +107,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 21:05:08',
             text: 'Per fortuna!',
             status: 'sent'
           },
@@ -130,7 +130,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 09:12:08',
             text: 'Per fortuna!',
             status: 'sent'
           },
@@ -153,7 +153,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 19:55:08',
             text: 'Per fortuna!',
             status: 'sent'
           },
@@ -176,7 +176,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 14:55:08',
             text: 'Va bene',
             status: 'sent'
           },
@@ -199,7 +199,7 @@ const app = new Vue({
             status: 'received'
           },
           {
-            date: '15/11/2021 15:55:08',
+            date: '15/11/2021 13:55:08',
             text: 'Per fortuna!',
             status: 'sent'
           },
@@ -208,10 +208,20 @@ const app = new Vue({
       }
     ],
     counter: 0,
+    chatPreview:''
   },
 
   methods: {
-
+    //funzione che, dato un parametro index, restituisce in output una porzione del testo dell'ultimo messaggio del contatto scelto, seguita da tre puntini di sospensione nel caso in cui la lunghezza dell'ultimo messaggio sia superiore a 8 caratteri
+    showChatPreview(index){
+      let prev ='';
+      if (this.contacts[index].messages[this.contacts[index].messages.length-1].text.length <= 8 ){
+        prev = this.contacts[index].messages[this.contacts[index].messages.length-1].text 
+      } else {
+        prev = this.contacts[index].messages[this.contacts[index].messages.length-1].text.substring(0,8)+"..."
+      }
+      return prev
+    }
   }
 
 });
