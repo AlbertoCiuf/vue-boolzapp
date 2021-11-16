e012be944773244ab2c7c010ffbd180ce1c5f902
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-16 09:51:40 +0100

    perfezionato overflow chat list, aggiunta min width generale, perfezionata search bar // inizio milestone-2

---
+++ milestone-2/js/script.js
@@ -0,0 +1,222 @@
+const app = new Vue({
+
+  el: '#app',
+
+  data: {
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: 'img/avatar_1.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Fabio',
+        avatar: 'img/avatar_2.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Samuele',
+        avatar: 'img/avatar_3.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Luigi',
+        avatar: 'img/avatar_4.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Ugo',
+        avatar: 'img/avatar_5.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Alessandra',
+        avatar: 'img/avatar_6.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Francesco',
+        avatar: 'img/avatar_7.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Michelangelo',
+        avatar: 'img/avatar_8.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      },
+      {
+        name: 'Michelangelo',
+        avatar: 'img/avatar_8.jpg',
+        visible: true,
+        messages: [
+          {
+            date: '15/11/2021 15:50:08',
+            text: 'Sei andato dal dottore?',
+            status: 'sent'
+          },
+          {
+            date: '15/11/2021 15:51:08',
+            text: 'Sìsì, tutto bene',
+            status: 'received'
+          },
+          {
+            date: '15/11/2021 15:55:08',
+            text: 'Per fortuna!',
+            status: 'sent'
+          },
+
+        ]
+      }
+    ],
+    counter: 0,
+  },
+
+  methods: {
+
+  }
+
+});
